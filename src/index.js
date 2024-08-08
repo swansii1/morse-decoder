7922aa985f4aa076e43ac2cc1fded328386c74d5
Matvey Lebedev <matvey.swansii@gmail.com>
2024-08-08 13:10:42 +0300

    feat: completed the task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+        let decodedString = '';
+      
+        for (let i = 0; i < expr.length; i += 10) {
+          const morseLetterCode = expr.slice(i, i + 10);
+
+
+          if (morseLetterCode === '**********') {
+            decodedString += ' ';
+
+          } else {
+            let morseCode = '';
+            for (let j = 0; j < 10; j += 2) {
+              const symbol = morseLetterCode.slice(j, j + 2);
+                if (symbol === '10') {
+                morseCode += '.';
+              } 
+                else if (symbol === '11') {
+                morseCode += '-';
+              }
+            }
+             decodedString += MORSE_TABLE[morseCode];
+          }
+        }
+          return decodedString;
+      }
 
 module.exports = {
     decode
